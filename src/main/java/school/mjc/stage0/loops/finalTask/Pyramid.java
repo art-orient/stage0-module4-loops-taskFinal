42391d6828d6fa756a8e5d8512ada2290f20c6e6
Aliaksandr Artsikhovich <art-orient@tut.by>
2022-07-11 21:02:29 +0300

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 0; j < cathetusLength * 2; j++) {
+                if (j >= cathetusLength + 1 - i && j <= cathetusLength) {
+                    System.out.print(cathetusLength - j + 1);
+                } else if (j > cathetusLength && j <= cathetusLength + i - 1) {
+                    System.out.print(j - cathetusLength + 1);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
